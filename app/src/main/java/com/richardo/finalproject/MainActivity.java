719f7c592a28b2980a4d6f1e38fedc11ea2ca019
Richardo Wijaya <richardowijaya772@gmail.com>
2023-01-16 17:16:50 +0700

    first commit

---
+++ app/src/main/java/com/richardo/finalproject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.richardo.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (menu instanceof MenuBuilder) {
+            ((MenuBuilder) menu).setOptionalIconsVisible(true);
+        }
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.appbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
