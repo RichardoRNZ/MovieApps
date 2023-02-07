a65f9786a6973ed669e539acb2723697ff36e194
RichardoRNZ <richardowijaya772@gmail.com>
2023-02-07 14:07:58 +0700

    final commit

--- app/src/main/java/com/richardo/finalproject/Activity/DashboardActivity.java
+++ app/src/main/java/com/richardo/finalproject/Activity/DashboardActivity.java
@@ -3,6 +3,8 @@ package com.richardo.finalproject.Activity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,7 +21,8 @@ public class DashboardActivity extends AppCompatActivity {
     private ImageView Profile;
     private TextView username;
     private FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private Button button_logout;
 
     @SuppressLint("CheckResult")
     @Override
@@ -28,7 +31,7 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         Profile = findViewById(R.id.profile_picture);
         username = findViewById(R.id.username);
-
+        button_logout = findViewById(R.id.btn_logout);
        username.setText(firebaseUser.getDisplayName());
        if(firebaseUser.getPhotoUrl()!=null)
        {
@@ -41,7 +44,15 @@ public class DashboardActivity extends AppCompatActivity {
                    .load(R.drawable.ic_baseline_person_2_24)
                    .into(Profile);
        }
+       button_logout.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               firebaseAuth.signOut();
+               startActivity(new Intent(DashboardActivity.this,LoginActivity.class));
+           }
+       });
     }
+
     public void onBackPressed() {
         startActivity(new Intent(this,MainActivity.class));
     }
